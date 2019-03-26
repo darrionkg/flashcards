c7813e4796f7e9000b4859ed9cb5c2b4ed5efdec
Darrion Gering and Justin Kerntz <justin@>
2019-03-26 16:09:10 -0700

    add terms and definitions and add grid style

--- js/scripts.js
+++ js/scripts.js
@@ -28,20 +28,164 @@ $(document).ready(function()  {
     $("#term3").removeClass();
     $("#define3").addClass("hide");
   });
-  // $(".clickable2").click(function()  {
-  //   $("#define1").addClass("hide");
-  //   $("#term1").removeClass();
-  // });
-  //
-  // $(".clickable1").click(function()  {
-  //   $("#define2").removeClass();
-  //   $("#term2").addClass("hide");
-  // });
-  //
-  // $(".clickable2").click(function()  {
-  //   $("#define2").addClass("hide");
-  //   $("#term2").removeClass();
-  // });
 
+  $("#term4.clickable1").click(function()  {
+    $("#define4").removeClass();
+    $("#term4").addClass("hide");
+  });
 
+  $("#define4.clickable2").click(function()  {
+    $("#term4").removeClass();
+    $("#define4").addClass("hide");
+  });
+
+  $("#term5.clickable1").click(function()  {
+    $("#define5").removeClass();
+    $("#term5").addClass("hide");
+  });
+
+  $("#define5.clickable2").click(function()  {
+    $("#term5").removeClass();
+    $("#define5").addClass("hide");
+  });
+
+  $("#term6.clickable1").click(function()  {
+    $("#define6").removeClass();
+    $("#term6").addClass("hide");
+  });
+
+  $("#define6.clickable2").click(function()  {
+    $("#term6").removeClass();
+    $("#define6").addClass("hide");
+  });
+
+  $("#term7.clickable1").click(function()  {
+    $("#define7").removeClass();
+    $("#term7").addClass("hide");
+  });
+
+  $("#define7.clickable2").click(function()  {
+    $("#term7").removeClass();
+    $("#define7").addClass("hide");
+  });
+
+  $("#term8.clickable1").click(function()  {
+    $("#define8").removeClass();
+    $("#term8").addClass("hide");
+  });
+
+  $("#define8.clickable2").click(function()  {
+    $("#term8").removeClass();
+    $("#define8").addClass("hide");
+  });
+
+  $("#term9.clickable1").click(function()  {
+    $("#define9").removeClass();
+    $("#term9").addClass("hide");
+  });
+
+  $("#define9.clickable2").click(function()  {
+    $("#term9").removeClass();
+    $("#define9").addClass("hide");
+  });
+
+  $("#term10.clickable1").click(function()  {
+    $("#define10").removeClass();
+    $("#term10").addClass("hide");
+  });
+
+  $("#define10.clickable2").click(function()  {
+    $("#term10").removeClass();
+    $("#define10").addClass("hide");
+  });
+
+  $("#term11.clickable1").click(function()  {
+    $("#define11").removeClass();
+    $("#term11").addClass("hide");
+  });
+
+  $("#define11.clickable2").click(function()  {
+    $("#term11").removeClass();
+    $("#define11").addClass("hide");
+  });
+
+  $("#term12.clickable1").click(function()  {
+    $("#define12").removeClass();
+    $("#term12").addClass("hide");
+  });
+
+  $("#define12.clickable2").click(function()  {
+    $("#term12").removeClass();
+    $("#define12").addClass("hide");
+  });
+
+  $("#term13.clickable1").click(function()  {
+    $("#define13").removeClass();
+    $("#term13").addClass("hide");
+  });
+
+  $("#define13.clickable2").click(function()  {
+    $("#term13").removeClass();
+    $("#define13").addClass("hide");
+  });
+
+  $("#term14.clickable1").click(function()  {
+    $("#define14").removeClass();
+    $("#term14").addClass("hide");
+  });
+
+  $("#define14.clickable2").click(function()  {
+    $("#term14").removeClass();
+    $("#define14").addClass("hide");
+  });
+
+  $("#term15.clickable1").click(function()  {
+    $("#define15").removeClass();
+    $("#term15").addClass("hide");
+  });
+
+  $("#define15.clickable2").click(function()  {
+    $("#term15").removeClass();
+    $("#define15").addClass("hide");
+  });
+
+  $("#term16.clickable1").click(function()  {
+    $("#define16").removeClass();
+    $("#term16").addClass("hide");
+  });
+
+  $("#define16.clickable2").click(function()  {
+    $("#term16").removeClass();
+    $("#define16").addClass("hide");
+  });
+
+  $("#term17.clickable1").click(function()  {
+    $("#define17").removeClass();
+    $("#term17").addClass("hide");
+  });
+
+  $("#define17.clickable2").click(function()  {
+    $("#term17").removeClass();
+    $("#define17").addClass("hide");
+  });
+
+  $("#term18.clickable1").click(function()  {
+    $("#define18").removeClass();
+    $("#term18").addClass("hide");
+  });
+
+  $("#define18.clickable2").click(function()  {
+    $("#term18").removeClass();
+    $("#define18").addClass("hide");
+  });
+
+  $("#term19.clickable1").click(function()  {
+    $("#define19").removeClass();
+    $("#term19").addClass("hide");
+  });
+
+  $("#define19.clickable2").click(function()  {
+    $("#term19").removeClass();
+    $("#define19").addClass("hide");
+  });
 });
